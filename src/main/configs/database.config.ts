b837fb90fbe1449bfa6567facbb72f662f950d52
MMCamargo <matheusmarianodecamargo@gmail.com>
2023-03-09 21:16:09 -0300

    Fix: database entities & migrations path.

--- src/main/configs/database.config.ts
+++ src/main/configs/database.config.ts
@@ -1,10 +1,36 @@
 import 'dotenv/config';
+import {
+	AdminEntity,
+	RecruiterEntity,
+	ApplicantsEntity,
+	JobEntity,
+	ApplicantPerPlaceEntity,
+} from './../../app/shared/database/entities';
+import {
+	CreateAdminsTable1678404037402,
+	CreateRecruitersTable1678404145659,
+	CreateApplicantsTable1678404239894,
+	CreateJobsTable1678406445266,
+	CreateApplicantsPerPlaceTable1678406520894,
+} from './../../app/shared/database/migrations';
 import { DataSource } from 'typeorm';
 
 export const dataSource = new DataSource({
 	type: 'postgres',
 	url: process.env.DATABASE_URL,
 	synchronize: false,
-	entities: ['src/app/shared/database/entities/**/*.ts'],
-	migrations: ['src/app/shared/database/migrations/**/*.ts'],
+	entities: [
+		AdminEntity,
+		RecruiterEntity,
+		ApplicantsEntity,
+		JobEntity,
+		ApplicantPerPlaceEntity,
+	],
+	migrations: [
+		CreateAdminsTable1678404037402,
+		CreateRecruitersTable1678404145659,
+		CreateApplicantsTable1678404239894,
+		CreateJobsTable1678406445266,
+		CreateApplicantsPerPlaceTable1678406520894,
+	],
 });
